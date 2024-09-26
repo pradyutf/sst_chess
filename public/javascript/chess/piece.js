afb51c6157c693d78049942f60e62255f05efe39
its-harshitgoel <its.harshitgoel@gmail.com>
2024-09-26 18:12:33 +0530

    Alert-when-king-is-killed

--- public/javascript/chess/piece.js
+++ public/javascript/chess/piece.js
@@ -1,58 +1,64 @@
-var Piece = function(config){
-    this.position = config.position;
-    this.color = config.color;
-    this.board = config.board;
-    if(this.position){
-        this.render();        
-    }    
-}
-Piece.prototype.moveTo = function(targetPosition){
-    console.log("Method not implemeted by: " + this.type);
-}
-
-
-Piece.prototype.attachListeners = function(){
-    //To be implemented
-}
-
-Piece.prototype.render = function(){
-    var col = this.position[0];
-    var row = this.position[1];
-    // Find the li element with matching data-col and data-row attributes
-    var element = document.querySelector(`[data-col="${col}"] [data-row="${row}"]`);
-    if (element) {  
-        // Remove the existing piece element from the DOM if it exists
-        if (this.$el && this.$el.parentNode) {
-            this.$el.parentNode.removeChild(this.$el);
-        }
-        // Create a new div element to represent the piece
-        var pieceElement = document.createElement('div');
-        
-        // Add classes to the new element for styling
-        pieceElement.classList.add('piece', this.color, this.type);
-        
-        // Clear any existing content in the cell
-        element.innerHTML = '';
-        
-        // Append the new piece element to the cell
-        element.appendChild(pieceElement);
-        this.$el = pieceElement;
-        this.attachListeners();
-    } else {
-        console.warn(`Element not found for position: ${this.position}`);
-    }
-}
-
-Piece.prototype.kill = function(targetPiece){
-    if (targetPiece && targetPiece.board) {
-        targetPiece.board.removePiece(targetPiece);
-    } else {
-        console.warn('Invalid target piece for kill operation');
-    }
-}
-
-Piece.prototype.updatePosition = function(targetPosition) {
-    this.position = targetPosition.col + targetPosition.row;
+var Piece = function (config) {
+  this.position = config.position;
+  this.color = config.color;
+  this.board = config.board;
+  if (this.position) {
     this.render();
-}
+  }
+};
+Piece.prototype.moveTo = function (targetPosition) {
+  console.log("Method not implemeted by: " + this.type);
+};
 
+Piece.prototype.attachListeners = function () {
+  //To be implemented
+};
+
+Piece.prototype.render = function () {
+  var col = this.position[0];
+  var row = this.position[1];
+  // Find the li element with matching data-col and data-row attributes
+  var element = document.querySelector(
+    `[data-col="${col}"] [data-row="${row}"]`
+  );
+  if (element) {
+    // Remove the existing piece element from the DOM if it exists
+    if (this.$el && this.$el.parentNode) {
+      this.$el.parentNode.removeChild(this.$el);
+    }
+    // Create a new div element to represent the piece
+    var pieceElement = document.createElement("div");
+
+    // Add classes to the new element for styling
+    pieceElement.classList.add("piece", this.color, this.type);
+
+    // Clear any existing content in the cell
+    element.innerHTML = "";
+
+    // Append the new piece element to the cell
+    element.appendChild(pieceElement);
+    this.$el = pieceElement;
+    this.attachListeners();
+  } else {
+    console.warn(`Element not found for position: ${this.position}`);
+  }
+};
+Piece.prototype.kill = function (targetPiece) {
+  if (targetPiece && targetPiece.board) {
+    targetPiece.board.removePiece(targetPiece);
+
+    if (targetPiece.type === "king") {
+      alert(`${this.color} has won!`);
+      
+      //tentative way to refresh page
+      location.reload(true);
+    }
+  } else {
+    console.warn("Invalid target piece for kill operation");
+  }
+};
+
+Piece.prototype.updatePosition = function (targetPosition) {
+  this.position = targetPosition.col + targetPosition.row;
+  this.render();
+};
