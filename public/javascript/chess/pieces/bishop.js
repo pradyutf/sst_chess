e2008123421c179d51d96e0baae7a5df2a0e1479
pradyutf <pradyutfogla@gmail.com>
2024-09-26 15:52:29 +0530

    bishop implement

--- public/javascript/chess/pieces/bishop.js
+++ public/javascript/chess/pieces/bishop.js
@@ -1,11 +1,52 @@
-var Bishop = function(config){
+// Bishop Class
+var Bishop = function(config) {
     this.type = 'bishop';
-    this.constructor(config);
+    Piece.call(this, config);
 };
 
+Bishop.prototype = Object.create(Piece.prototype);
+Bishop.prototype.constructor = Bishop;
 
+Bishop.prototype.moveTo = function(targetPosition) {
+    let newCol = targetPosition.col.charCodeAt(0) - 65;
+    let newRow = parseInt(targetPosition.row);
+    let oldCol = this.position.charCodeAt(0) - 65;
+    let oldRow = parseInt(this.position[1]);
 
-Bishop.prototype = new Piece({});
-Bishop.prototype.move = function(newPosition){
+    let colDiff = Math.abs(newCol - oldCol);
+    let rowDiff = Math.abs(newRow - oldRow);
 
-}
+    // Check if the move is diagonal
+    if (colDiff === rowDiff) {
+        // Check if the path is clear
+        let colStep = newCol > oldCol ? 1 : -1;
+        let rowStep = newRow > oldRow ? 1 : -1;
+        for (let i = 1; i < colDiff; i++) {
+            let checkPos = {
+                col: String.fromCharCode(oldCol + i * colStep + 65),
+                row: (oldRow + i * rowStep).toString()
+            };
+            if (this.board.getPieceAt(checkPos)) {
+                console.log("Path is not clear");
+                return false;
+            }
+        }
+
+        let targetPiece = this.board.getPieceAt(targetPosition);
+        if (targetPiece && targetPiece.color === this.color) {
+            console.log("Cannot capture own piece");
+            return false;
+        }
+
+        if (targetPiece) {
+            this.kill(targetPiece);
+        }
+
+        this.updatePosition(targetPosition);
+        this.board.changeTurns();
+        return true;
+    }
+
+    console.log("Invalid move for Bishop");
+    return false;
+};
